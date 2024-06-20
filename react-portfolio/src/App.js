ad75fcf3ef6867e242f88302633bf79dbaf80fd7
OteIan <oteian9@gmail.com>
2024-06-20 17:26:30 +0300

    Fix: import error

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch, Routes} from 'react-router-dom';
+import Navbar from './Pages/Home/Navbar';
 import Home from './Pages/Home/Homescreen';
 import NotFound from './Pages/Home/Errors/NotFound';
 
